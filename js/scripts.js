4fc8713b91367715cab4cc885bb79cfd42a0ed60
Brendan Hellar and dylan crocker <brendan@>
2019-04-11 11:14:42 -0700

    add a second player

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,17 @@
 
 
 var jouster = new Image();
+var jouster2 = new Image();
 jouster.src = "img/Jouster.png";
-player1PosX = 0;
-player1PosY = 0;
+jouster2.src = "img/Jouster2.png";
+player1PosX = 350;
+player1PosY = 450;
 player1VelX = 0;
 player1VelY = 0;
+player2PosX = 0;
+player2PosY = 450;
+player2VelX = 0;
+player2VelY = 0;
 friction = .98;
 moveSpeed = 1.5;
 jumpSpeed = 1.5;
@@ -43,6 +49,23 @@ $().ready(function() {
         player1VelY--;      }
 
     }
+    if(keys[65]){
+      if(player2VelX > -moveSpeed){
+        player2VelX--;
+      }
+    //right
+    }
+    if (keys[68]){
+      if(player2VelX < moveSpeed){
+        player2VelX++;
+      }
+    //up
+    }
+    if (keys[87]){
+      if (player2VelY > -jumpSpeed) {
+        player2VelY--;      }
+
+    }
     physics();
     context.clearRect(0,0, canvas.width, canvas.height);
     //draw background
@@ -50,7 +73,7 @@ $().ready(function() {
     context.fillRect(0,0,canvas.width, canvas.height);
     //draw players
     context.drawImage(jouster, player1PosX+=player1VelX, player1PosY+=player1VelY, 50, 50);
-
+    context.drawImage(jouster2, player2PosX+=player2VelX, player2PosY+=player2VelY, 50, 50);
   }
 
   update();
@@ -71,7 +94,15 @@ $().ready(function() {
       player1PosY = 450;
       player1VelY = 0
     }
+    if(player2PosY <= 450){
+      player2VelY+=.098; //gravity
+    }
+    else{
+      player2PosY = 450;
+      player2VelY = 0
+    }
     player1VelX*=friction; //friction
+    player2VelX*=friction; //friction
 
   }
 });
