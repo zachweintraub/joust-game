32fc789eb5fdf5f81f66dbc1d4af4dad5d67b5d1
Brendan Hellar and dylan crocker <brendan@>
2019-04-11 16:10:32 -0700

    add a background and an overlay

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,16 @@ var jouster1Left = new Image();
 var jouster1Right = new Image();
 var jouster2Left = new Image();
 var jouster2Right = new Image();
-
+var joustLogo = new Image();
+var background = new Image();
 //Grabbing the source for the images.
 jouster1Left.src = "img/Jouster1Left.png";
 jouster1Right.src = "img/Jouster1Right.png";
 jouster2Left.src = "img/Jouster2Left.png";
 jouster2Right.src = "img/Jouster2Right.png";
-
+joustLogo.src = "img/Logo.gif";
+background.src = "img/background.png";
+var blink = true;
 //Set the jouster as a variable
 var jouster = jouster1Left;
 var jouster2 = jouster2Right;
@@ -42,7 +45,9 @@ $().ready(function() {
   //Getting the canvas to work on and allow us to write
   canvas = $("#canvas").get(0);
   context = canvas.getContext("2d");
-
+  //update();
+  drawMain();
+});
   // This functions allows for the update function to run on repeat
   function update(){
     requestAnimationFrame(update);
@@ -66,23 +71,59 @@ $().ready(function() {
   }
 
   //We call the update function
-  update();
 
   // These are the event listeners that allow for the user to experience no lag time on movement
   document.addEventListener('keydown', function(e){
     keys[e.keyCode] = true;
+    if(e.keyCode == 40){
+      e.preventDefault();
+    }
+    //Enter Key to kick off update loop
+    if(e.keyCode == 13){
+      update();
+      clearInterval(blinkInterval);
+      keys[13] = false;
+
+    }
   });
   document.addEventListener('keyup', function(e){
     keys[e.keyCode] = false;
   });
 
   // This is the function to draw the game
+  function drawMain() {
+    context.fillStyle = "black";
+    context.fillRect(0,0, canvas.width, canvas.height);
+    context.drawImage(joustLogo, canvas.width/2-joustLogo.width/2, canvas.height/2-joustLogo.height);
+    blinkInterval = setInterval(blinkText, 500);
+  }
+
+
+  function blinkText(){
+
+    if(!blink){
+      context.font ="30px Fantasy";
+      context.fillStyle = "white";
+      context.fillText("Press Enter", canvas.width/2-80, canvas.height/2+60);
+      console.log("fill text");
+      blink = true;
+    }
+    else if(blink){
+
+      console.log("fill black")
+      context.fillStyle = "black";
+      context.fillRect(canvas.width/2-100, canvas.height/2+30, 200, 35);
+      blink = false;
+    }
+  }
+
   function drawGame(){
 
     context.clearRect(0,0, canvas.width, canvas.height);
     //draw background
     context.fillStyle = "black";
     context.fillRect(0,0,canvas.width, canvas.height);
+    context.drawImage(background,0, 0, canvas.width, canvas.height);
     //draw players
     context.drawImage(jouster, player1PosX+=player1VelX, player1PosY+=player1VelY, 50, 50);
     context.drawImage(jouster2, player2PosX+=player2VelX, player2PosY+=player2VelY, 50, 50);
@@ -135,31 +176,34 @@ $().ready(function() {
   }
 
   // This is the physics functionality
+
+
+  var floor = 425;
+  var ceiling = 0;
+
   function physics(){
 
-    if(player1PosY <= 450){
+    if(player1PosY <= floor){
       player1VelY+=.098; //gravity
     }
     else{
-      player1PosY = 450;
+      player1PosY = floor;
       player1VelY = 0
     }
-    if(player2PosY <= 450){
+    if(player2PosY <= floor){
       player2VelY+=.098; //gravity
     }
     else{
-      player2PosY = 450;
+      player2PosY = floor;
       player2VelY = 0
     }
-    if(player1PosY <= 0){
+    if(player1PosY <= ceiling){
       player1PosY = 0
     }
-    if(player2PosY <= 0){
+    if(player2PosY <= ceiling){
       player2PosY = 0
     }
     player1VelX*=friction; //friction
     player2VelX*=friction; //friction
 
   }
-
-});
