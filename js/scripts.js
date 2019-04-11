87af2479fdb38320923e5dd9187a3f17aca93647
Brendan Hellar and dylan crocker <brendan@>
2019-04-11 10:53:33 -0700

    smooth the movement and allow for graviry elements

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,75 @@
 
 var jouster = new Image();
 jouster.src = "img/Jouster.png";
-playerPosX = 0;
-playerPosY = 0;
+player1PosX = 0;
+player1PosY = 0;
+player1VelX = 0;
+player1VelY = 0;
+friction = .98;
+moveSpeed = 1.5;
+jumpSpeed = 1.5;
+
+keys=[];
+
 
 $().ready(function() {
 
   canvas = $("#canvas").get(0);
   context = canvas.getContext("2d");
-  setInterval(update, 1000/144)
 
 
   function update(){
+    requestAnimationFrame(update);
 
-    gravity();
 
+    //HANDLE CONTROL INPUT
+    //left
+    if(keys[37]){
+      if(player1VelX > -moveSpeed){
+        player1VelX--;
+      }
+    //right
+    }
+    if (keys[39]){
+      if(player1VelX < moveSpeed){
+        player1VelX++;
+      }
+    //up
+    }
+    if (keys[38]){
+      if (player1VelY > -jumpSpeed) {
+        player1VelY--;      }
+
+    }
+    physics();
+    context.clearRect(0,0, canvas.width, canvas.height);
+    //draw background
     context.fillStyle = "black";
     context.fillRect(0,0,canvas.width, canvas.height);
-
-    context.drawImage(jouster,playerPosX,playerPosY, 50, 50);
+    //draw players
+    context.drawImage(jouster, player1PosX+=player1VelX, player1PosY+=player1VelY, 50, 50);
 
   }
 
-  document.addEventListener('keydown', move);
+  update();
+  document.addEventListener('keydown', function(e){
+    keys[e.keyCode] = true;
 
-  function gravity(){
-    playerPosY++;
-    if(playerPosY>=450){
-      playerPosY = 450;
+  });
+  document.addEventListener('keyup', function(e){
+    keys[e.keyCode] = false;
+  });
+
+  function physics(){
+
+    if(player1PosY <= 450){
+      player1VelY+=.098; //gravity
     }
-  }
-
-
-  function move(event){
-    let key = event.keyCode;
-    if(key == 37){
-      playerPosX-=20;
-    }else if (key == 39){
-      playerPosX+=20;
+    else{
+      player1PosY = 450;
+      player1VelY = 0
     }
+    player1VelX*=friction; //friction
+
   }
 });
